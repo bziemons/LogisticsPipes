3ba61f1a96df0c9a003653009731009ab1c7fad7
theZorro266 <root@theZorro266.com>
2016-03-07 18:49:35 +0100

    Switched all input/output stream occurrences

---
+++ common/logisticspipes/network/packets/debuggui/DebugInfoUpdate.java
@@ -0,0 +1,86 @@
+package logisticspipes.network.packets.debuggui;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+import network.rs485.logisticspipes.util.LPDataInput;
+import network.rs485.logisticspipes.util.LPDataOutput;
+
+import logisticspipes.network.abstractpackets.ModernPacket;
+import logisticspipes.ticks.DebugGuiTickHandler;
+import logisticspipes.ticks.DebugGuiTickHandler.VarType;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Accessors(chain = true)
+public class DebugInfoUpdate extends ModernPacket {
+
+	@Getter
+	@Setter
+	private Integer[] path;
+
+	@Getter
+	@Setter
+	private VarType information;
+
+	public DebugInfoUpdate(int id) {
+		super(id);
+	}
+
+	@Override
+	public void readData(LPDataInput data) throws IOException {
+		ByteArrayInputStream bis = new ByteArrayInputStream(data.readLengthAndBytes());
+		ObjectInput in = new ObjectInputStream(bis);
+		try {
+			information = (VarType) in.readObject();
+		} catch (ClassNotFoundException e) {
+			throw new UnsupportedOperationException(e);
+		}
+		int size = data.readInt();
+		path = new Integer[size];
+		for (int i = 0; i < size; i++) {
+			path[i] = data.readInt();
+		}
+	}
+
+	@Override
+	public void processPacket(EntityPlayer player) {
+		try {
+			DebugGuiTickHandler.instance().handleContentUpdatePacket(path, getInformation());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void writeData(LPDataOutput data) throws IOException {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutput out = new ObjectOutputStream(bos);
+		out.writeObject(getInformation());
+		byte[] bytes = bos.toByteArray();
+		data.writeLengthAndBytes(bytes);
+		data.writeInt(path.length);
+		for (Integer element : path) {
+			data.writeInt(element);
+		}
+	}
+
+	@Override
+	public ModernPacket template() {
+		return new DebugInfoUpdate(getId());
+	}
+
+	@Override
+	public boolean isCompressable() {
+		return true;
+	}
+}
---
+++ common/logisticspipes/network/packets/debuggui/DebugSetVarContent.java
@@ -0,0 +1,69 @@
+package logisticspipes.network.packets.debuggui;
+
+import java.io.IOException;
+
+import network.rs485.logisticspipes.util.LPDataInput;
+import network.rs485.logisticspipes.util.LPDataOutput;
+
+import logisticspipes.network.abstractpackets.ModernPacket;
+import logisticspipes.ticks.DebugGuiTickHandler;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Accessors(chain = true)
+public class DebugSetVarContent extends ModernPacket {
+
+	@Getter
+	@Setter
+	private String content;
+
+	@Getter
+	@Setter
+	private Integer[] path;
+
+	public DebugSetVarContent(int id) {
+		super(id);
+	}
+
+	@Override
+	public void readData(LPDataInput data) throws IOException {
+		content = data.readUTF();
+		int size = data.readInt();
+		path = new Integer[size];
+		for (int i = 0; i < size; i++) {
+			path[i] = data.readInt();
+		}
+	}
+
+	@Override
+	public void processPacket(EntityPlayer player) {
+		try {
+			DebugGuiTickHandler.instance().handleVarChangePacket(path, content, player);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void writeData(LPDataOutput data) throws IOException {
+		data.writeUTF(content);
+		data.writeInt(path.length);
+		for (Integer element : path) {
+			data.writeInt(element);
+		}
+	}
+
+	@Override
+	public ModernPacket template() {
+		return new DebugSetVarContent(getId());
+	}
+
+	@Override
+	public boolean isCompressable() {
+		return true;
+	}
+}
